94b6fda1e9691300cf8b5a0ef0b1d663c8672cd0
root <184812659+Velatryx@users.noreply.github.com>
2025-12-26 06:50:11 -0500

    1

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk 'BEGIN{ORS=""}/^(Registrant|Admin|Tech)/{split($0,a,":");v=a[2];sub(/^ /,"",v);if(a[1]~/Ext$/)a[1]=a[1]":";print(n++?"\n":"") a[1] ", " v}' > "$1".csv
+whois "$1" | awk 'BEGIN{ORS=""}/^(Registrant|Admin|Tech)/{split($0,a,":");v=a[2];sub(/^ /,"",v);if(a[1]~/Ext$/&&v=="")v=" ";if(v=="")v=" ";print(n++?"\n":"") a[1] ", " v}' > "$1".csv
