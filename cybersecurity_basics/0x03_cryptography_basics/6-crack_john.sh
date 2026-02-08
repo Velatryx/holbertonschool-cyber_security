af231905d385dc9dfc47d1821225976f94292fd6
Murx <184812659+Velatryx@users.noreply.github.com>
2026-02-08 20:25:33 +0400

    Update 6-crack_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=Raw-SHA256 "$1" | awk -F: '{print $2}' > 6-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-SHA256 "$1" && john --show --format=Raw-SHA256 "$1" | head -n -2 | cut -d: -f2 > 6-password.txt
