95c0869585fbad9db6a461d67e8f512e966c4144
Murx <184812659+Velatryx@users.noreply.github.com>
2026-03-18 18:29:44 +0400

    Update 6-custom_scan.sh

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap --scanflags URGACKPSHRSTSYNFINECECWR $1 -p $2 -oN custom_scan.txt 2>&1 /dev/null
+sudo nmap --scanflags URGACKPSHRSTSYNFINECECWR $1 -p $2 -oN custom_scan.txt &> /dev/null
