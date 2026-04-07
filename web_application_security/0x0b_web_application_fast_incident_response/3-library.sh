153332e23523ae320426ee8dd9c59b8e52b4f6fc
Murad <184812659+Velatryx@users.noreply.github.com>
2026-04-07 18:09:05 +0400

    3

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -2,4 +2,4 @@
 
 file=logs.txt
 
-awk '{print $12}' $file | sort | uniq -c | sort -rn | head -1 | awk '{print $2}'
+awk '{print $12}' $file | sort | uniq -c | sort -rn | head -1 | tr -d '"' | awk '{print $2}' 
