b342316898c49e5b193e09d17ae9ede2ef2e3c91
Murx <184812659+Velatryx@users.noreply.github.com>
2026-04-07 17:47:15 +0400

    Update 1-endpoint.sh

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 file=logs.txt
-awk '{print $7}' | sort | uniq -c | sort -r | head -1 | awk '{print $2}'
+awk '{print $7}' "$file" | sort | uniq -c | sort -r | head -1 | awk '{print $2}'
