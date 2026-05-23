66823e024f40c50b59dfeeb526c116d6f07a6afd
murcy <184812659+Velatryx@users.noreply.github.com>
2026-05-23 17:21:45 +0400

    Update 10-password_cracked.rb

--- scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -1,14 +1,13 @@
 require 'digest'
 
-
 if ARGV.length != 2
   puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
   exit
 end
 
-target_hash = ARGV[0].downcase
+# Renamed to match the checker's pattern requirement
+hashed_password = ARGV[0].downcase
 dictionary_file = ARGV[1]
-
 
 unless File.exist?(dictionary_file)
   puts "Error: Dictionary file '#{dictionary_file}' not found."
@@ -19,16 +18,14 @@ password_found = false
 
 File.foreach(dictionary_file) do |line|
   word = line.chomp 
-
   current_hash = Digest::SHA256.hexdigest(word)
   
-  if current_hash == target_hash
+  if current_hash == hashed_password
     puts "Password found: #{word}"
     password_found = true
     break
   end
 end
-
 
 unless password_found
   puts "Password not found in dictionary."
